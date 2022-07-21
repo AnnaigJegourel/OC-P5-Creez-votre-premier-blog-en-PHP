98e80039ea9f42629eb9c3047cc98c3349dbf6b9
Annaig Jégourel <a_jegourel@yahoo.fr>
2022-07-21 16:32:48 +0200

    Create MainController.php

---
+++ src/Controller/MainController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+/**
+ * Class MainController
+ * Manages the Main Features
+ * @package App\Controller
+ */
+abstract class MainController
+{
+    /**
+     * @var Environment|null
+     */
+    protected $twig = null;
+
+    /**
+     * MainController constructor
+     * Creates the Template Engine & adds its Extensions
+     */
+    public function __construct()
+        {
+            $this->twig = new Environment(new FilesystemLoader("../src/View"), array("cache"=>false));
+        }
+    
+    /**
+     * Redirects to another URL
+     * @param string $page
+     * @param array $params
+     */
+    public function redirect(string $page, array $params = [])
+    {
+        $params["access"] = $page;
+        header("Location: index.php?" . http_build_query($params));
+
+        exit;
+    }
+};
