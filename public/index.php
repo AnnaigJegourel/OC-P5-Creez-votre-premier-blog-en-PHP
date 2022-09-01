fd8774547ab5ffafec4f104be614ed357516096f
Annaig Jégourel <a_jegourel@yahoo.fr>
2022-09-01 13:49:43 +0200

    update index to make session start

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ require_once "../vendor/autoload.php";
 
 use Tracy\Debugger;
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 Debugger::enable();
 
 $router = new App\Router();
