18993449cf4d69bf070a3e803d0164692c45c49f
Annaig Jégourel <a_jegourel@yahoo.fr>
2022-09-02 08:57:37 +0200

    Create CommentController.php

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Factory\ModelFactory;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class CommentController extends MainController {
+}
