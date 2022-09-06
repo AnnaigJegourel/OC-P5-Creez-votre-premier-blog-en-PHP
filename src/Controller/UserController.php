4331c6bd5f01e8ba06611a184bb4c3a3c60167a1
Annaig Jégourel <a_jegourel@yahoo.fr>
2022-09-06 11:24:47 +0200

    Create UserController.php with defaultMethod (renders login page)

---
+++ src/Controller/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\Factory\ModelFactory;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Class UserController
+ * manages the User functions
+ * @package App\Controller
+ */
+class UserController extends MainController
+{
+    /**
+     * Renders the Login Form View
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function defaultMethod()
+    {
+        return $this->twig->render("login.twig");
+    }
+}
