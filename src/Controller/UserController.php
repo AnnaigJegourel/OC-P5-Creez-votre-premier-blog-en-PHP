8f1ceaf4713bfc732669c6f9889929d63bc2139b
Annaig Jégourel <a_jegourel@yahoo.fr>
2022-09-07 09:35:45 +0200

    Create All Users List view, getter & read method
    Create userslist.twig & update home view, create getAllUsers() & readAllUsers()

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -25,4 +25,33 @@ class UserController extends MainController
     {
         return $this->twig->render("login.twig");
     }
-}
+
+    /**
+     * Gets all Users
+     * @return array\string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function getAllUsers()
+    {
+        $allUsers = ModelFactory::getModel("User")->listData();
+
+        return $allUsers;
+    }
+
+    /**
+     * Renders the View of all Users
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function readAllUsersMethod()
+    {
+        $allUsers = $this->getAllUsers();
+
+        return $this->twig->render("userslist.twig", ["allUsers" => $allUsers]);
+    }
+
+} 
