1de93345c833640a021e6193935068214367071c
Annaig Jégourel <a_jegourel@yahoo.fr>
2022-09-18 13:47:37 +0200

    Create contactMethod() for Home form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -25,4 +25,24 @@ class HomeController extends MainController
     {
         return $this->twig->render("home.twig");
     }
+
+    /**
+     * Treats contact form data
+     * Sends data to admin e-mail address
+     * @return void
+     */
+    public function contactMethod()
+    {
+        $to = "a_jegourel@yahoo.fr";
+        $subject= addslashes($this->getPost()["choice"]);
+        /* $message = "Line 1\r\nLine 2\r\nLine 3"; */
+        $text = addslashes($this->getPost()["message"]);
+        $name = addslashes($this->getPost()["name"]);
+        $reply_to = addslashes($this->getPost()["email"]);
+
+        mail($to, $subject, $text, $name, $reply_to);
+        $message = "Votre message a bien été envoyé. Nous vous répondrons dès que possible.";
+        /* oui enfin non y a rien qui arrive dans ma boîte mail (spams compris) */
+        return $this->twig->render("message.twig", ["message" => $message]);
+    }
 }
