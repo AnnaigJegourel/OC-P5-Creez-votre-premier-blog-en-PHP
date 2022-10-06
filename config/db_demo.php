64146ef59412e0d102d9008f3e3e347c9d716d6f
Annaig Jégourel <a_jegourel@yahoo.fr>
2022-10-06 11:56:41 +0200

    Create mailing constants in db.sql & put file in gitignore

---
+++ config/db_demo.php
@@ -0,0 +1,18 @@
+<?php
+
+//FOR DATABASE
+define("DB_DSN", "mysql:host=localhost;dbname=p5-blog"); //replace "localhost" & "p5-blog"
+define("DB_USER", "database_username");  // replace "database_username"
+define("DB_PASS", "database_user_password");  // replace "database_user_password"
+define("DB_OPTIONS", array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+// FOR MAIL
+define("MAIL_HOST", "mail.host.com"); // replace "mail.host.com"
+define("MAIL_PORT", 000); // replace "000"
+define("MAIL_FROM", "mail@host.com"); // replace "mail@host.com"
+define("MAIL_PASSWORD", "mail-user-password"); // replace "mail-user-password"
+define("MAIL_USERNAME", "mail-username"); // replace "mail-username"
+define("MAIL_TO", "mail@host.com"); // replace "mail@host.com"
+define("MAIL_TO_NAME", "mail-to-name"); // replace "mail-to-name"
+define("MAIL_CC", "mail@host.com"); // replace "mail@host.com"
+define("MAIL_CC_NAME", "mail-cc-name"); // replace "mail-cc-name"
