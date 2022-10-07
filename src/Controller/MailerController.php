31ca30239702d5f053a2c98c6a16d178ca410d02
Annaig Jégourel <a_jegourel@yahoo.fr>
2022-10-07 15:00:37 +0200

    Remove putSlashes() from Mailer

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -38,12 +38,13 @@ class MailerController extends MainController
     {
         require_once "../config/db.php";
 
+        // Encoder en utf-8? Caractères spéciaux non lus dans emails reçus
+        // utf8_encode() & _decode() sont obsolètes
         $data = [       
-            "name" => $this->putSlashes($this->getPost()["name"]),
-            "email" => $this->putSlashes($this->getPost()["email"]),
-            "subject" => $this->putSlashes($this->getPost()["subject"]),
-            //"message" => $this->putSlashes($this->getPost()["message"])
-            "message" => $this->getPost()["message"]    //ok mais pas sécu?
+            "name" => $this->getPost()["name"],
+            "email" => $this->getPost()["email"],
+            "subject" => $this->getPost()["subject"],
+            "message" => $this->getPost()["message"]
         ];
 
         try{
@@ -52,48 +53,23 @@ class MailerController extends MainController
             $this->mail->isSMTP();                     
             $this->mail->Host       = MAIL_HOST;    
     
-            $this->mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $this->mail->SMTPAuth   = true;                               
             $this->mail->Username   = MAIL_FROM;      
             $this->mail->Password   = MAIL_PASSWORD;                          
-            $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;        
         
-            /**
-            * To send email with XAMPP, use the PEAR Mail and Net_SMTP packages, which allow you to send email using an external SMTP account (such as a Gmail account). Follow these steps:
-            * Install the Mail and Net_SMTP PEAR modules: 
-            */
-            $this->mail->Port       = MAIL_PORT;                               //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            $this->mail->Port       = MAIL_PORT;      
 
-            //To load the French version
-            /*$this->mail->setLanguage('fr', '/optional/path/to/language/directory/');*/
-
-            //Expéditeur
             $this->mail->setFrom(MAIL_FROM, MAIL_USERNAME); 
+            $this->mail->addAddress(MAIL_TO, MAIL_TO_NAME);
 
-            //Destinataires
-            $this->mail->addAddress(MAIL_TO, MAIL_TO_NAME);     //Name is optional 
-            //ajouter qqn en copie
-            $this->mail->addCC(MAIL_CC, MAIL_CC_NAME);
-            //ajouter qqn en copie cachée
-            //$this->mail->addBCC('bcc@example.com');
-
-            // Répondre à : email et nom saisis dans form
             $this->mail->addReplyTo($data["email"], $data["name"]);
 
-            //Attachments
-            /* $this->mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-            $this->mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name*/
-
-            //Content
-            /*    $this->mail->isHTML(true); */                  //Set email format to HTML
-            // sujet et message saisis dans form                    
             $this->mail->Subject = $data["subject"];
             $this->mail->Body    = $data["message"];
             $this->mail->AltBody = $data["message"];
             
-            //On envoie
             $this->mail->send();
-            
-
 
             // Affichage sur le site en cas de succès:
             echo "Message envoyé";                      
