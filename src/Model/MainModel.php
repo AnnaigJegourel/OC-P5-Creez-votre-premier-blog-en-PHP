42cb2f4092414a7b34b4408bdcdc2f04d765539b
Annaig Jégourel <a_jegourel@yahoo.fr>
2022-11-03 12:44:14 +0100

    Add PHPDoc comment to listSinglePostComments()

--- src/Model/MainModel.php
+++ src/Model/MainModel.php
@@ -80,7 +80,13 @@ abstract class MainModel
         return $this->database->getAllData($query);
     }
 
-    /* **************** tous les comm DUN POST avec user name from latest ***************** */
+    /**
+     * Lists all comments about a post
+     *
+     * @param string|null $value
+     * @param string|null $key
+     * @return array|mixed
+     */
     public function listSinglePostComments(string $value = null, string $key = null)
     {
         if (isset($key)) {
@@ -99,17 +105,6 @@ abstract class MainModel
         return $this->database->getAllData($query);
     }
     
-    /* **************** tous les comm avec user name from latest ***************** */
-    /*public function listAllComments()
-    {
-        $query = "SELECT * FROM Comment
-                INNER JOIN User ON Comment.user_id = User.id
-                ORDER BY Comment.date_created DESC";
-
-        return $this->database->getAllData($query);
-    }*/
-
-
     /**
      * Creates a new Data entry
      * 
